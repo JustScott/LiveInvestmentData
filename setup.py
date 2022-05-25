5161fa2c35e779650c0928f0ab8e100ed079a726
JustScott <>
2022-05-25 15:36:49 -0500

    Adjusted the setup.py version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 with open("README.md", "r") as file:
     long_description = file.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Simple to use stock, commodity, forex, and cryptocurrency market webscraper, utilizing the beautiful soup and requests libraries.'
 
 # Setting up
